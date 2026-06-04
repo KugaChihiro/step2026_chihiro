a72727b23bd00bf931edbaa6479fc28a848b41e8
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2026-06-04 23:36:09 +0900

    feat : solve third problem (module)

--- modularized_calculator.py
+++ modularized_calculator.py
@@ -33,6 +33,14 @@ def read_divided(line, index):
     token = {'type': 'DIVIDED'}
     return token, index + 1
 
+def read_left_parentheses(line, index):
+    token = {'type': 'L_PARENTHESES'}
+    return token, index + 1
+
+def read_right_parentheses(line, index):
+    token = {'type': 'R_PARENTHESES'}
+    return token, index + 1
+
 def tokenize(line):
     tokens = []
     index = 0
@@ -47,6 +55,10 @@ def tokenize(line):
             (token, index) = read_times(line, index)
         elif line[index] == '/':
             (token, index) = read_divided(line, index)
+        elif line[index] == '(':
+            (token, index) = read_left_parentheses(line, index)
+        elif line[index] == ')':
+            (token, index) = read_right_parentheses(line, index)
         else:
             print('Invalid character found: ' + line[index])
             exit(1)
@@ -104,12 +116,47 @@ def evaluate_addition_subtraction(tokens):
         index += 1
     return answer
 
+# （）の処理を行う
+# 1. 現時点で一番内側かつ左側にある()で囲まれた範囲を特定する
+# 2. 1の範囲内で四則演算を実行
+# 3. 四則演算の結果をもとに、tokensを更新 : ((1+2)*2+1)*2 -->  (3*2+1)*2
+
+def evaluate_parentheses(tokens):
+    index = 0
+    while index < len(tokens):  # tokensのリストを先頭から走査。一番初めに見つかった")"について考える。
+        if (tokens[index]['type'] == 'R_PARENTHESES'):
+            new_index = index
+            while 0 <= new_index < len(tokens): # ")"が一番初めにindexから、反対方向にさかのぼり、"("を探す。
+                if tokens[new_index]['type'] == "L_PARENTHESES":
+                    answer = evaluate_4_arithmetic_operations(tokens[new_index+1:index]) # ()のセットが見つかった場合、その（）内で四則演算を行う。
+                    new_token = {'type':'NUMBER','number': answer} # 四則演算の結果をtokenとして追加。不要な箇所は削除。
+                    del tokens[new_index:index+1]
+                    tokens.insert(new_index,new_token)
+                    return tokens
+                new_index -= 1
+        index += 1
+
+# 四則演算を行う
+# 掛け算・割り算→足し算・引き算
+def evaluate_4_arithmetic_operations(tokens):
+    result_multiplication_division = evaluate_multiplication_division(tokens)
+    answer = evaluate_addition_subtraction(result_multiplication_division)
+    return answer
+
+# カッコが1つでも残っていれば、カッコ内を計算（evaluate_parentheses）して、最初からやり直す（再帰）
+# カッコがすべて消えたら、通常の四則演算を行って最終結果を返す
+def evaluate(tokens):
+    if any(token.get('type') == 'L_PARENTHESES' for token in tokens):
+        tokens = evaluate_parentheses(tokens)
+        return evaluate(tokens)
+    else:
+        answer = evaluate_4_arithmetic_operations(tokens)
+        return answer
 
 
 def test(line):
     tokens = tokenize(line)
-    result_multiplication_division = evaluate_multiplication_division(tokens)
-    actual_answer = evaluate_addition_subtraction(result_multiplication_division)
+    actual_answer = evaluate(tokens)
     expected_answer = eval(line)
     if abs(actual_answer - expected_answer) < 1e-8:
         print("PASS! (%s = %f)" % (line, expected_answer))
@@ -134,6 +181,10 @@ def run_test():
     test("0.1+1")
     test("1+0.1")
     test("1.0+2.1-3")
+    test("(1+2)")
+    test("(1+2*3)")
+    test("((1+2)*3)")
+    test("(1+2)*(1+3)")
     print("==== Test finished! ====\n")
 
 run_test()
@@ -142,8 +193,4 @@ while True:
     print('> ', end="")
     line = input()
     tokens = tokenize(line)
-    print(tokens)
-    result_multiplication_division = evaluate_multiplication_division(tokens)
-    print(result_multiplication_division)
-    answer = evaluate_addition_subtraction(result_multiplication_division)
-    print("answer = %f\n" % answer)
+    evaluate(tokens)
